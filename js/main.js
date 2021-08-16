7db05d0c29a17967d0d8f40797a0c73b5dc20421
Manavesh Narendra <manavesh.narendra@gmail.com>
2021-08-16 16:56:22 +0530

    Corrected the main js file

---
+++ js/main.js
@@ -0,0 +1,11 @@
+
+
+
+var hiddenMessage = '<p><span>\u2606</span>Superstar</p>';
+var i = 0;
+
+for (i = 0; i < 10; i++) {
+  document.write(hiddenMessage);
+};
+
+// the star is invalid character , replaced by /u2606 as the crrect code for the special character
